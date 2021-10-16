be147013e9ef5df00e3ed1a307decdb599b6bd22
Yuri <iury.fedoroff2018@yandex.ru>
2021-10-16 22:01:37 +0300

    test-1 убираем дубликаты

---
+++ src/main/java/com/fedorov/dto/Project.java
@@ -0,0 +1,51 @@
+package main.java.com.fedorov.dto;
+/*
+ *
+ *@Data 16.10.2021
+ *@autor Fedorov Yuri
+ *@project testJava
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс для хранения информации о проекте и входящих в него пакетах.
+ */
+public class Project {
+    //имя проекта
+    private final String name;
+    //список пакетов проекта
+    private final List<Package> packages = new ArrayList<>();
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Проверяет наличие циклических зависимостей между пакетами проекта.
+     * Когда мы говорим "A зависит от B (или A -> B)", имеется в виду, что пакет B
+     * содержится списке зависимостей (dependencies) пакета A.
+     * Например, есть проект1, в нем задан список пакетов - packages = [A]
+     * при зависимостях A->B->C->A – цикл есть
+     * при зависимостях A->B->D, A->C->D – цикла нет
+     * <p>
+     * Обращаем ваше внимание, что в packages у проекта не обязательно находятся все пакеты. В данном примере там находится только один пакет A.
+     */
+    public Boolean hasCyclicDependencies() {
+        return false;
+    }
+
+    /**
+     * Упорядоченный список пакетов проекта для компиляции с учетом их
+     * зависимостей. Скомпилировать пакет мы можем только после успешной компиляции его зависимостей.
+     * Например, для проекта A->B->C, D->C
+     * порядок компиляции может быть C, B, A, D или C, D, B, A или C, B, D, A
+     */
+    public List<Package> getCompilationOrder() {
+
+        return null;
+    }
+}
+
---
+++ src/main/java/com/fedorov/util/DistinctUtil.java
@@ -0,0 +1,29 @@
+package main.java.com.fedorov.util;
+/*
+ *
+ *@Data 16.10.2021
+ *@autor Fedorov Yuri
+ *@project testJava
+ *
+ */
+
+import main.java.com.fedorov.dto.Package;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DistinctUtil {
+    public static int[] task1(int[] a) {
+        return Arrays.stream(a).peek(item->{
+            if(item<0)
+                throw new IllegalArgumentException("массив содержит значение меньше 0");
+        }).distinct().toArray();
+    }
+    public static List<Package> task1(List<Package> packages) {
+        return packages.stream().peek(item->{
+            if(item.getName().isEmpty())
+                throw new IllegalArgumentException("массив содержит значение меньше 0");
+        }).distinct().collect(Collectors.toList());
+    }
+    }
